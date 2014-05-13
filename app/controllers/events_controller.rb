311964817351cbc3a2a8c6cfa28b79f485138f5d
Stephanie Raad <stephy.raad@gmail.com>
2014-05-13 16:53:27 -0700

    third commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+
+   before_action :authenticate_user
+
   def index
   	@events= Event.all
   end
@@ -38,5 +41,4 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
-
-end #end of class
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ CalendarApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'events#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
